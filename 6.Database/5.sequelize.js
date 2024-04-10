9f68b4369d5f37e8386ae78a14cbf3fb490d9ab1
Rushikeshkadu998 <rushikesh.kadu@thinkitive.com>
2024-04-10 19:24:02 +0530

    Third Commit

---
+++ 6.Database/5.sequelize.js
@@ -0,0 +1,11 @@
+/*
+
+1) Sequelize is an ORM (Object Relational Mapper) library for NodeJS.
+2) Sequelize provides an easy-to use interface with relational databases in ExpressJS
+3) It supports various database systems such as MySQL,postgreSQL,SQLite etc.
+4) we need to install this module
+   > npm install sequelize;
+
+5) 
+
+*/
