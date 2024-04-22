a7b312e3cea9da5917cd760b9231ad0886357083
Rushikeshkadu998 <rushikesh.kadu@thinkitive.com>
2024-04-22 15:42:34 +0530

    Day8 Updated

--- 17.Understanding Async request/Project/app.js
+++ 17.Understanding Async request/Project/app.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const product = require('./routes/product')
 
-
-
 app.use('/products',product)
 app.listen(8000,()=>{
     console.log("Listening on port 8000")
