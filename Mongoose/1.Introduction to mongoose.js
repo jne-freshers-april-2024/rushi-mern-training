dec31aace302b6e324076a54f276a840755faf0f
Rushikeshkadu998 <rushikesh.kadu@thinkitive.com>
2024-04-22 15:27:22 +0530

    day5 updated

--- Mongoose/1.Introduction to mongoose.js
+++
@@ -1,8 +0,0 @@
-/*
-
-1) Mongoose is a package in expressJS
-2) Mongoose provides ease of performing CRUD operation in expressJS
-3) Prerequisit to use mongoose is nodeJS and mongodb
-4) > npm i mongoose
-
-*/
--- Mongoose/Project 1/app.js
+++ Mongoose/Project 1/app.js
@@ -1,90 +1,81 @@
 const { json } = require('express');
 const mongoose = require('mongoose');
+const connectDB = require('./DB/connectDB')
+const studentModel = require('./models/student')
+const url = "mongodb://localhost:27017/Temp";
 
-
-const connectDb = ()=>{
-        return mongoose.connect("mongodb://localhost:27017/Temp")
+connectDB(url)
+.then(response =>{
+    console.log("Connected To DB")
+})
+.catch()
+{
+    console.log("Error While connecting with DB");
 }
 
+
 const readData = async ()=>{
-    let DB = await connectDb();
-
-    let studentSchema = new mongoose.Schema({
-        name : { type:String, required:true, trim:true},
-        salary : { type:mongoose.Decimal128, required:true},
-        age : { type:Number,required:true, min:18, max:50},
-    });    
-
-    const studentModel = mongoose.model('Student',studentSchema)
+   try{
     let data = await studentModel.find()
     console.log(data)
-    
+   }
+   catch(err)
+   {
+       console.log("Error:",err);
+   }
 }
 
 
 const insertData = async ()=>{
+    try{
+        const studentDoc = studentModel({
+            name:"RK",
+            salary:60000.12,
+            age:23,
+        })
 
-    let DB = await connectDb();
-
-    // Defining Model
-    let studentSchema = new mongoose.Schema({
-        name : { type:String, required:true, trim:true},
-        salary : { type:mongoose.Decimal128, required:true},
-        age : { type:Number,required:true, min:18, max:50},
-    });
-
-    //Compiling Schema
-    const studentModel = mongoose.model('Student',studentSchema)
-    const studentDoc = studentModel({
-        name:"RK",
-        salary:60000.12,
-        age:23,
-    })
-
-    const data = await studentDoc.save()
-    console.log(data)
+        const data = await studentDoc.save()
+        console.log(data)
+    }
+    catch(err)
+    {
+        console.log("Error While Inserting Data:",err)
+    }
 }
 
-const updateData =async ()=>{
-    let DB = await connectDb();
-
-    let studentSchema = new mongoose.Schema({
-        name : { type:String, required:true, trim:true},
-        salary : { type:mongoose.Decimal128, required:true},
-        age : { type:Number,required:true, min:18, max:50},
-    });    
-
-    const studentModel = mongoose.model('Student',studentSchema)
-    const updatedStudent = await studentModel.findByIdAndUpdate('661936abfd25e2be3cb59f69', { name:"Sujay",salary:30000,age:23 }, { new: true });
-    console.log(updateStudent)
+const updateData =async (id)=>{
+    try{
+        const updatedStudent = await studentModel.findByIdAndUpdate(id, { name:"Sujay",salary:30000,age:23 }, { new: true });   // id -> '661936abfd25e2be3cb59f69'
+        console.log(updateStudent)
+    }
+    catch(err)
+    {
+        console.log("Error While Updating Data:",err)
+    }
 }
 
 
-const deleteData = async () => {
-
-        let DB = await connectDb();
-
-        let studentSchema = new mongoose.Schema({
-            name : { type: String, required: true, trim: true },
-            salary : { type: mongoose.Decimal128, required: true },
-            age : { type: Number, required: true, min: 18, max: 50 },
-        });    
-
-        const studentModel = mongoose.model('Student', studentSchema);
-
-        const deletedStudent = await studentModel.findOneAndDelete({ name: "Sujay" });
+const deleteData = async (name) => {
+    try{
+        const deletedStudent = await studentModel.findOneAndDelete({ name: name });
 
         if (deletedStudent) {
             console.log('Deleted student:', deletedStudent);
         } else {
             console.log('Student not found');
         }
-   
+    }
+    catch(err)
+    {
+        console.log("Error While Deleting Data:",err)
+    }
 }
 
+
 // deleteData();
-
-
 // readData()
 // insertData()
 // updateData()
+
+
+
