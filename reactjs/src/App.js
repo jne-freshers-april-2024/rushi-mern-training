68e0909d3e587314c776d8d82f360c3d2f302bf4
Rushikeshkadu998 <rushikesh.kadu@thinkitive.com>
2024-04-24 12:49:09 +0530

    useCallback

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import Counter from "./components/CounterApp";
 // import ActiveUsers from './components/ActiveUsers';
-import Dashboard from "./components/Dashboard";
+// import Dashboard from "./components/Dashboard";
 
 function App() {
-  return <Dashboard />;
+  return( 
+  // <Dashboard />;
+  <Counter/>
+)
 }
 
 export default App;
